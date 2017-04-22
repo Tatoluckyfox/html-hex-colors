e426aba7f79e5d4057d7ffa0da0fe0631c096180
Tato Luckyfox <Tatoluckyfox@users.noreply.github.com>
2017-04-22 11:33:20 +0200

    Added duration

--- colors.py
+++ colors.py
@@ -1,7 +1,12 @@
+import time
+
 fil = open("colors.html", "w")
 
 def ex(n):
     return '#' + str(hex(n)).split('x')[1]
+
+print "starting"
+t = time.time()
 
 fil.write("<html>\n <head>\n<title>HTML Hex colors</title>\n    </head>\n   <body>\n        <table>")
 for x in range(1048576, 16777215):
@@ -10,3 +15,9 @@ for x in range(1048576, 16777215):
     
 fil.write('     </table>\n      <a href="https://github.com/tatoluckyfox/html-hex-colors">github.com/tatoluckyfox/html-hex-colors</a>\n </body>\n</html>')
 fil.close()
+
+t2 = time.time()
+final_time = t2 - t
+
+print "duration: {0} seconds\nexiting in 5 seconds".format(final_time)
+time.sleep(5)
